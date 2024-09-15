bd7bd8f1f61091760f8110b347e9583bbc19b8cf
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2024-09-15 10:49:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// item er bitor li ke ber kora//
+
+//  let list =  document.getElementsByTagName("li");
+//  for(let lis of list){
+//     console.log(lis);
+//  }
+
+
+// h1 heading er html ber kora//
+
+// let heading = document.getElementsByTagName("h1");
+//  for(let hed of heading) {
+//     hed.innerHTML;
+//     console.log(hed);
+//  }
+
+
+// heding title color and size change kora//
+//   let heading  = document.getElementsByTagName("h3");
+//   for(let hed of heading) {
+//     hed.innerText;
+//     hed.style.color= "orange";
+//     hed.style.fontSize = "25px"
+//   }
+
+
+// ............................................................//
+
+        //  getElementsByClassName
+
+//   let lis = document.getElementsByClassName("col");
+//   for(let man of lis) {
+//     man.style.color= "blue"
+//     man.style.fontSize = "18px"
+//   }
